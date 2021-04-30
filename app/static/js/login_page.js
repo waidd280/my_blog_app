b09196af201d4df1dd560d249f87f5ffcccf49ec
Waid Dunlop <wddunlop@outlook.com>
2021-04-30 10:35:29 -0400

    Add js function to check user entry on login

--- app/static/js/login_page.js
+++ app/static/js/login_page.js
@@ -1,3 +1,14 @@
 function greet(){
     alert("Welcome to BlogApp");
 }
+
+function checkEntry(){
+    var email, password;
+
+    email = document.getElementById("email_entry").value;
+    password = document.getElementById("password_entry").value;
+
+    if (email === "" || password === ""){
+        alert("All fields must be filled");
+    }
+}
