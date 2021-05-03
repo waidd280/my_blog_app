c2e5e58b98487fccc2ce528670ee2d868deebffd
Waid Dunlop <wddunlop@outlook.com>
2021-05-03 13:29:11 -0400

    update models add questions and answers columns

--- app/models.py
+++ app/models.py
@@ -7,3 +7,18 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    questions = db.relationship('Question', backref='user', lazy=True)
+    answers = db.relationship('Answer', backref='user', lazy=True)
+
+
+class Question(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    question_text = db.Column(db.String(250), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    answers = db.relationship('Answer', backref='question', lazy=True)
+
+
+class Answer(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    answer_text = db.Column(db.String(800), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
