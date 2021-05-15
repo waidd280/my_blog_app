7ebedebedab58340c995016132e6f384ae55845d
Waid Dunlop <wddunlop@outlook.com>
2021-05-15 17:14:59 -0400

    Add backend to display question page

--- app/question_routes.py
+++ app/question_routes.py
@@ -1,7 +1,9 @@
 from flask import Blueprint
+from .models import Question
 
 questions = Blueprint('questions', __name__, url_prefix="/questions")
 
-@questions.route('/practice-question')
-def test_q():
-    return "Test question page"
+@questions.route('/<question_id>')
+def test_q(question_id):
+    page_question = Question.query.filter_by(id=question_id).first()
+    return "Question Title: " + page_question.question_title
