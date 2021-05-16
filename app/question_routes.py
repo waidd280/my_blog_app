7093be933c0beedda02c3ad519ace11b90d39977
Waid Dunlop <wddunlop@outlook.com>
2021-05-16 09:13:53 -0400

    Add css to question display on each question page

--- app/question_routes.py
+++ app/question_routes.py
@@ -1,9 +1,12 @@
-from flask import Blueprint
-from .models import Question
+from flask import Blueprint, render_template
+from .models import Question, User
 
 questions = Blueprint('questions', __name__, url_prefix="/questions")
 
 @questions.route('/<question_id>')
 def test_q(question_id):
     page_question = Question.query.filter_by(id=question_id).first()
-    return "Question Title: " + page_question.question_title
+    question_author = User.query.filter_by(id=page_question.user_id).first().name
+
+    return render_template('question_page.html', pagetitle=page_question.question_title, 
+    page_question=page_question, question_author=question_author)
